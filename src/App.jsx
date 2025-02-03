465852244160abfd27199879e80d0ae2c595cd65
Adebowalem Adedayo <adeniceadebo@gmail.com>
2025-02-04 00:05:56 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
@@ -29,7 +30,7 @@ function App() {
           activePlayerSymbol={activePlayer}
         />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
